684f140a31f5e87c04be498143ccf145192643f0
JW <wenroujing@hotmail.com>
2021-02-17 15:31:02 -0500

    finished the required feature

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jing Wen on 2/13/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
